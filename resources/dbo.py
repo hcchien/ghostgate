217088438a9c403dd5ae53d219e60d80ecae20ae
ChienHsin-Chan <hcchien@gmail.com>
2016-01-30 15:33:53 +0800

    allow the parameter for query

--- resources/dbo.py
+++ resources/dbo.py
@@ -6,8 +6,11 @@ class model_resource(Resource):
     def __init__(self):
         self.dbi = DBI()
         pass
-    def get(self):
-        rv = self.dbi.query_db("select * from " + self._table)
+    def get(self, id = ''):
+        if (id != ''):
+            rv = self.dbi.query_db("select * from " + self._table + " where id = ?", [id])
+        else:
+            rv = self.dbi.query_db("select * from " + self._table)
         return json.dumps(rv)
     def post(self):
         pass
